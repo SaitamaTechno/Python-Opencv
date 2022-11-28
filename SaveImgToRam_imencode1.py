2b5d3eecb73af6afe93b71d269fd1d0d4547c42b
SaitamaTechno <95487121+SaitamaTechno@users.noreply.github.com>
2022-11-28 15:15:41 +0300

    Update SaveImgToRam_imencode1.py

--- SaveImgToRam_imencode1.py
+++ SaveImgToRam_imencode1.py
@@ -8,7 +8,11 @@ img=cv2.imencode(".jpg", img)[1]
 print(img)
 byte_img = img.tobytes()
 print(len(byte_img))
-
+"""
+f=open("resim/tfm.jpg", "rb") #You can also directly read bytes from image
+byte_img=f.read()
+f.close()
+"""
 recvd_img=np.array(bytearray(byte_img), dtype="uint8")
 recvd_img=cv2.imdecode(recvd_img, cv2.IMREAD_COLOR)
 cv2.imshow("sa", recvd_img)
